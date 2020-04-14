6cf1f8bb5d0f0886aca8ab38d8f19c8a61e48be7
Sparksys98 <60644023+Sparksys98@users.noreply.github.com>
2020-04-14 11:45:19 +0300

    done

--- project/urls.py
+++ project/urls.py
@@ -1,18 +1,4 @@
-"""project URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from petra import views
@@ -20,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('product-list/', views.ProductList.as_view(), name="product-list"),
     path('product-detail/<int:product_id>',
-         views.ProductDetail.as_view(), name="product-detail")
+         views.ProductDetail.as_view(), name="product-detail"),
+    path('signup/', views.Register.as_view(), name="signup"),
+    path('login/', views.Login.as_view(), name="login"),
 ]
